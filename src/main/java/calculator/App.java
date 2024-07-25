4d10954c62aeffeadf3cc3e03526dafb848095e5
t2024-m0158 <t2024-m0158@t2024-m0158ui-MacBookAir.local>
2024-07-25 21:13:59 +0900

    docs:클래스 분류

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,12 +1,14 @@
 package calculator;
 
 import java.util.*;
+import java.lang.*;
 
 public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Double> list = new ArrayList<>(); // 연산된 값을 list에 저장
+        Calculator calculator = new Calculator();
 
         while (true) {
 
@@ -18,37 +20,17 @@ public class App {
 
             sc.nextLine(); // num1, num2 뒤의 줄 바꿈 문자 처리
             System.out.println("사칙연산의 기호를 입력하세요 (+, -, *, /)");
-            char operator = sc.nextLine().charAt(0);
+            String operator = sc.nextLine();
 
-            double result = 0;
-            boolean validOperation = true; //유효하지 않은 연산자를 구별하기 위해 boolean타입을 사용
-
-            // 사칙연산 switch
-            switch (operator) {
-                case '+':
-                    result = num1 + num2; // 더하기 연산
-                    break;
-                case '-':
-                    result = num1 - num2; // 빼기 연산
-                    break;
-                case '*':
-                    result = num1 * num2; // 곱하기 연산
-                    break;
-                case '/':  // 나누기 연산
-                    if (num2 != 0) { // 0으로 나눌 수 없음
-                        result = num1 / num2;
-                    } else {
-                        System.out.println("0으로 나눌 수 없습니다. 다시 입력하세요.");
-                        validOperation = false;
-                    }
-                    break;
-                default:
-                    System.out.println("유효하지 않은 연산자입니다. 다시 입력하세요.");
-                    validOperation = false; // 유효하지 않은 연산자
-                    break;
+            if(operator.length() !=1){
+                System.out.println("유효하지 않은 연산자입니다. 다시 입력하세요");
+                continue;
             }
 
-            if (!validOperation) {
+
+            double result = calculator.calculate(operator, num1, num2);
+
+            if (Double.isNaN(result)) {
                 continue; // 오류가 발생했으므로 루프의 시작으로 돌아감
             }
 
