83d4c03ac93b762101749f8a3cde88776c264a6b
t2024-m0158 <t2024-m0158@t2024-m0158ui-MacBookAir.local>
2024-07-26 18:04:42 +0900

    feat: circlecalculator

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -9,56 +9,99 @@ public class App {
         Calculator calculator = new Calculator();
 
         while (true) {
-            System.out.println("첫 번째 숫자를 입력하세요:");
-            double num1 = sc.nextDouble();
+            System.out.println("사칙연산 or 원의 넓이 (press: 1 사칙연산, press: 2 원의 넓이, exit: 종료)");
+            String choosemath = sc.nextLine();
 
-            System.out.println("두 번째 숫자를 입력하세요:");
-            double num2 = sc.nextDouble();
+            if (choosemath.equals("1")) {
+                while (true) {
+                    System.out.println("첫 번째 숫자를 입력하세요:");
+                    double num1 = sc.nextDouble();
 
-            sc.nextLine(); // num1, num2 뒤의 줄 바꿈 문자 처리
-            System.out.println("사칙연산의 기호를 입력하세요 (+, -, *, /)");
-            String operator = sc.nextLine();
+                    System.out.println("두 번째 숫자를 입력하세요:");
+                    double num2 = sc.nextDouble();
 
-            if (operator.length() != 1) {
-                System.out.println("유효하지 않은 연산자입니다. 다시 입력하세요");
-                continue;
+                    sc.nextLine(); // num1, num2 뒤의 줄 바꿈 문자 처리
+                    System.out.println("사칙연산의 기호를 입력하세요 (+, -, *, /)");
+                    String operator = sc.nextLine();
+
+                    if (operator.length() != 1) {
+                        System.out.println("유효하지 않은 연산자입니다. 다시 입력하세요");
+                        continue;
+                    }
+
+                    double result = calculator.calculate(operator, num1, num2);
+
+                    if (Double.isNaN(result)) {
+                        continue; // 오류가 발생했으므로 루프의 시작으로 돌아감
+                    }
+
+                    System.out.println("계산 결과는: " + result);
+
+                    // 추가 명령어 처리
+                    System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력)");
+                    String std = sc.nextLine();
+                    if (std.equalsIgnoreCase("remove")) {
+                        calculator.removeResult();
+                        System.out.println("첫 번째 결과값이 삭제되었습니다.");
+                    }
+
+                    // 저장된 연산결과 조회
+                    System.out.println("저장된 연산결과를 조회 (inquiry 입력 시 조회)");
+                    String stf = sc.nextLine();
+                    if (stf.equalsIgnoreCase("inquiry")) {
+                        calculator.inquiryResult();
+                    }
+
+                    System.out.println("계속 진행하시겠습니까? (yes: 계속, no: 종료)");
+                    String continueChoice = sc.nextLine();
+                    if (continueChoice.equalsIgnoreCase("no")) {
+                        break; // 사칙연산 모드 종료
+                    }
+                }
+            } else if (choosemath.equals("2")) {
+                while (true) {
+                    System.out.println("반지름을 입력하세요:");
+                    double radius = sc.nextDouble();
+
+                    sc.nextLine(); // num1, num2 뒤의 줄 바꿈 문자 처리
+
+                    double crresult = calculator.calculateCircleAre(radius);
+
+                    if (Double.isNaN(crresult)) {
+                        continue; // 오류가 발생했으므로 루프의 시작으로 돌아감
+                    }
+
+                    System.out.println("계산 결과는: " + crresult);
+
+                    // 추가 명령어 처리
+                    System.out.println("가장 먼저 저장된 원 넓이 결과를 삭제하시겠습니까? (remove 입력)");
+                    String sta = sc.nextLine();
+                    if (sta.equalsIgnoreCase("remove")) {
+                        calculator.crremoveResult();
+                        System.out.println("첫 번째 결과값이 삭제되었습니다.");
+                    }
+
+                    // 저장된 원 넓이 결과 조회
+                    System.out.println("저장된 원 넓이 결과를 조회 (inquiry 입력 시 조회)");
+                    String stb = sc.nextLine();
+                    if (stb.equalsIgnoreCase("inquiry")) {
+                        calculator.crinquiryResult();
+                    }2
+
+                    System.out.println("계속 진행하시겠습니까? (yes: 계속, no: 종료)");
+                    String continueChoice = sc.nextLine();
+                    if (continueChoice.equalsIgnoreCase("no")) {
+                        break; // 원 넓이 모드 종료
+                    }
+                }
+            } else if (choosemath.equalsIgnoreCase("exit")) {
+                System.out.println("계산 종료");
+                break; // 전체 프로그램 종료
+            } else {
+                System.out.println("유효하지 않은 입력입니다. 다시 시도해 주세요.");
             }
-
-            double result = calculator.calculate(operator, num1, num2);
-
-            if (Double.isNaN(result)) {
-                continue; // 오류가 발생했으므로 루프의 시작으로 돌아감
-            }
-
-            System.out.println("계산 결과는: " + result);
-
-            // 추가 명령어 처리
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력)");
-            String std = sc.nextLine();
-            if (std.equalsIgnoreCase("remove")) {
-                calculator.removeResult();
-                System.out.println("첫 번째 결과값이 삭제되었습니다.");
-            }
-
-            // 저장된 연산결과 조회
-            System.out.println("저장된 연산결과를 조회 (inquiry 입력 시 조회)");
-            String stf = sc.nextLine();
-            if (stf.equalsIgnoreCase("inquiry")) {
-                calculator.inquiryResult();
-            }
-
-
-        // 종료 여부 확인
-        System.out.println("계산기 종료(exit 입력 시 종료)");
-        String str = sc.nextLine();
-        if (str.equalsIgnoreCase("exit")) {
-            System.out.println("계산 종료");
-            break;
-        } else {
-            System.out.println("계산 계속");
         }
-    }
 
         sc.close(); // Scanner 자원 해제
-}
+    }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,12 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-    private double lastResult; // 최근 계산 결과를 저장할 필드
-    private List<Double> resultList; // 결과를 저장할 리스트
+    private double lastResult; // 최근 사칙계산 결과를 저장할 필드
+    private double crlastResult;// 최근 원넓이 계산 결과를 저장할 필드
+    private List<Double> resultList; // 사칙연산 결과를 저장할 리스트
+    private List<Double> circleresultList; //원의 넓이 저장할 리스트
 
-    // 생성자: resultList 초기화
+    public static final double PI = 3.14159265358979323846;
+
+
+    // 생성자: resultList, circleresult 초기화
     public Calculator() {
         this.resultList = new ArrayList<>();
+        this.circleresultList = new ArrayList<>();
     }
 
     // 계산 메서드
@@ -43,23 +49,48 @@ public class Calculator {
             return Double.NaN; // Not a Number
         }
 
-        // 최근 계산 결과 저장
+        // 최근 사칙연산 계산 결과 저장
         this.lastResult = result;
         resultList.add(result); // 결과를 리스트에 저장
         return result;
     }
 
-    // Getter 메서드: lastResult 필드의 값을 반환
-    public double getLastResult() {
-        return lastResult;
-    }
+        public double calculateCircleAre (double radius) {
+            double circleresult = 0;
+            if (radius > 0) {
+                circleresult = radius * radius *PI;
+            } else {
+                System.out.println("유효하지 않은 연산자입니다. 다시 입력하세요.");
+                return Double.NaN; // 유효하지 않은 연산자
+            }
 
-    // Setter 메서드: lastResult 필드의 값을 설정
-    public void setLastResult(double lastResult) {
-        this.lastResult = lastResult;
-    }
 
-    // 결과 삭제 메서드
+            //최근 원넓이 계산 결과 저장
+            this.crlastResult = circleresult;
+            circleresultList.add(crlastResult);
+            return crlastResult;
+        }
+        // Getter 메서드: circleresultList의 값을 반환
+        public List<Double> getCircleresultList() {
+            return circleresultList;
+        }
+
+        // Setter 메서드 : circleresultList의 값을 반환
+        public void setCircleresultList(List<Double> circleresultList) {
+            this.circleresultList = circleresultList;
+        }
+
+        // Getter 메서드: lastResult 필드의 값을 반환
+        public double getLastResult() {
+            return lastResult;
+        }
+
+        // Setter 메서드: lastResult 필드의 값을 설정
+        public void setLastResult(double lastResult) {
+            this.lastResult = lastResult;
+        }
+
+    // 사칙연산 결과 삭제 메서드
     public void removeResult() {
         if (!resultList.isEmpty()) {
             resultList.remove(0); // 처음 저장된 결과를 삭제
@@ -68,7 +99,27 @@ public class Calculator {
         }
     }
 
-    // 저장된 결과 조회 메서드
+    //원넓이 결과 삭제 메서드
+    public void crremoveResult() {
+        if (!circleresultList.isEmpty()) {
+            circleresultList.remove(0); // 처음 저장된 결과를 삭제
+        } else {
+            System.out.println("삭제할 결과가 없습니다.");
+        }
+    }
+
+    //원넓이 저장된 결과 조회 메서드
+    public void crinquiryResult() {
+        if (circleresultList.isEmpty()) {
+            System.out.println("저장된 결과가 없습니다.");
+        } else {
+            System.out.println("저장된 연산결과:"); // 리스트에서 값을 불러온다
+            for (Double item : circleresultList) {
+                System.out.println(item);
+            }
+        }
+    }
+    // 사칙연산 저장된 결과 조회 메서드
     public void inquiryResult() {
         if (resultList.isEmpty()) {
             System.out.println("저장된 결과가 없습니다.");
